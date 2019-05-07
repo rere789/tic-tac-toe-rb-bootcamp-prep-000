14893acf4f73ac83d612ef3406d76d04406dfaac
Reina Ewing <reina.castro3@gmail.com>
2019-05-07 14:23:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -101,10 +101,10 @@ end
 # Define your play method below
 # 
 
-def play(board)
-  turn = 0
-  while turn < 9
-    turn(board)
-    turn += 1
-  end 
-end
+# def play(board)
+#   turn = 0
+#   while turn < 9
+#     turn(board)
+#     turn += 1
+#   end 
+# end
