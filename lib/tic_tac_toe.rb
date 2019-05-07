27858bf81022299bc0bc8a50d965e22ee038d700
Reina Ewing <reina.castro3@gmail.com>
2019-05-07 14:16:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,6 +47,16 @@ def turn(board)
   end
 end
 
+def turn_count(turns)
+  counter = []
+  turns.each do |turn|
+    if turn == "X" || turn == "O"
+      counter << 1 
+    end
+  end 
+  counter.length.to_i 
+end 
+
 def current_player(board)
     num = turn_count(board) 
    if num.even?
